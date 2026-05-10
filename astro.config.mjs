b32f56e328ebc12e437e4624eb8d1506f428d396
mmnvb <bobomurodm2004@gmail.com>
2026-05-10 11:21:59 +0500

    feat: removed comments, added link to playground

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,69 +1,70 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import fs from 'fs'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import fs from "fs";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'Kocha Lang',
-        favicon: '/icon.png',
-        customCss:[
-            './src/assets/styles/custom.css'
-        ],
-        expressiveCode: {
-            // themes: ['nord'],
-    shiki: {
-
-      langs: [
-        JSON.parse(fs.readFileSync('./src/assets/syntax/kocha.json', 'utf-8'))
+  integrations: [
+    starlight({
+      title: "Kocha Lang",
+      favicon: "/icon.png",
+      customCss: ["./src/assets/styles/custom.css"],
+      expressiveCode: {
+        // themes: ['nord'],
+        shiki: {
+          langs: [
+            JSON.parse(
+              fs.readFileSync("./src/assets/syntax/kocha.json", "utf-8"),
+            ),
+          ],
+        },
+      },
+      social: {
+        github: "https://github.com/kocha-lang/kocha",
+        codePen: "https://play.kocha-lang.uz",
+      },
+      sidebar: [
+        {
+          label: "Boshlash",
+          items: [
+            { label: "Salom dunyo", slug: "start/install" },
+            { label: "Qulay ishlash", slug: "start/extension" },
+          ],
+        },
+        {
+          label: "Syntax",
+          items: [
+            { label: "O'zgaruvchilar", slug: "syntax/variables" },
+            { label: "Shartlar", slug: "syntax/conditions" },
+            { label: "Aylanmalar", slug: "syntax/loops" },
+            { label: "Array va Object", slug: "syntax/collections" },
+            { label: "Funksiyalar", slug: "syntax/functions" },
+          ],
+        },
+        {
+          label: "Standard Lib",
+          items: [
+            { label: "I/O", slug: "std/io" },
+            { label: "Math", slug: "std/math" },
+          ],
+        },
+        {
+          label: "Misollar",
+          autogenerate: { directory: "examples" },
+        },
+        {
+          label: "Oxiri",
+          items: [{ label: "Credits", slug: "misc/credits" }],
+        },
       ],
-    },
-        },
-        social: {
-            github: 'https://github.com/kocha-lang/kocha',
-        },
-        sidebar: [
-            {
-                label: 'Boshlash',
-                items: [
-                    { label: 'Salom dunyo', slug: 'start/install' },
-                    { label: "Qulay ishlash", slug: 'start/extension' },
-                ],
-            },
-            {
-                label: 'Syntax',
-                items: [
-                    { label: "O'zgaruvchilar", slug: 'syntax/variables' },
-                    { label: 'Shartlar', slug: 'syntax/conditions' },
-                    { label: 'Aylanmalar', slug: 'syntax/loops' },
-                    { label: 'Array va Object', slug: 'syntax/collections' },
-                    { label: 'Funksiyalar', slug: 'syntax/functions' },
-                ],
-            },
-            {
-                label: 'Standard Lib',
-                items: [
-                    { label: 'I/O', slug: 'std/io' },
-                    { label: 'Math', slug: 'std/math' },
-                ],
-            },
-            {
-                label: 'Misollar',
-                autogenerate: { directory: "examples" }
-            },
-            {
-                label: 'Oxiri',
-                items: [
-                    { label: 'Credits', slug: 'misc/credits' },
-                ],
-            },
-        ],
-        components: {
-            Hero: './src/components/Hero.astro',
-            // Footer: './src/components/Footer.astro',
-        }
-		}), tailwind()],
+      components: {
+        Hero: "./src/components/Hero.astro",
+        // Footer: './src/components/Footer.astro',
+      },
+    }),
+    tailwind(),
+  ],
 });
